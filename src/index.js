92a659edd4cc95ba1c324d3695cde92347447012
milos381 <milos_popovic@outlook.com>
2020-12-29 18:20:32 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 console.log('%c HI', 'color: firebrick')
+let breeds = [];
+document.addEventListener('DOMContentLoaded', function () {
+    loadImages();
+    loadBreedOptions();
+});
+function loadImages() {
+    let imageUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imageUrl)
+    .then(resp => resp.json())
+    .then(json => addImages(json))
+    
+}
+function addImages(json) {
+    
+    let container = document.querySelector('#dog-image-container');
+    json.message.forEach(image => {
+        let newImageEl = document.createElement('img'); 
+        newImageEl.src = image;
+        container.appendChild(newImageEl);
+    });
+}
+function loadBreedOptions() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => addBreeds(json))
+}
+function addBreeds(json) {
+    let ul = document.querySelector('#dog-breeds');
+    let li = document.createElement('li');
+    
+    for (const key in Object.keys(json.message)) {
+        console.log(Object.keys(json.message)[key]);
+        li.innerText = Object.keys(json.message)[key];
+        ul.appendChild(li);
+    }
+    
+}
